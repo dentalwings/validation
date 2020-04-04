9849b164ccca8a3dd0ad9afc7e9fa96738bc7619
Joël SCHAAL <joel.schaal@dental-wings.com>
2020-04-04 00:46:04 -0400

    Use proper extension (.Tests.ps1)

--- Medit/DWOS-9.2-Tests.ps1
+++
@@ -1,9 +0,0 @@
-describe '7Series System 9.2' {
-    it 'has UAC enabled' {
-        (Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System).EnableLUA | should be 1
-    }
-	
-    it 'has not WSUS set up' {
-        Test-Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate' | Should Be $false
-    }
-}
