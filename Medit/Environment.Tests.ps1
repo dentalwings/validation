8376e864c788c9840b69512dc00c57a13d0c535d
sel-aaraj <45242315+sel-aaraj@users.noreply.github.com>
2020-11-06 17:52:36 -0500

    Update Environment.Tests.ps1

--- Medit/Environment.Tests.ps1
+++ Medit/Environment.Tests.ps1
@@ -7,4 +7,11 @@ describe 'a Windows system properly configured for Medit' {
         # https://github.com/dentalwings/validation/wiki/Medit-Windows-Configuration-WSUS
         (Test-Path HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate) | should be $false
     }
+
+    Context 'VCredist is present' {
+    	It 'VCredist 2019 is present' {
+	        (Get-WmiObject Win32_product -Filter "Name LIKE '%Microsoft Visual C++ 2019 X64%'" | Where-Object { $_.IdentifyingNumber }).Version | Should Be "14.27.29016"
+	    }
+    }
+
 }
