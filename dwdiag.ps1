7cc54dd058f0328f7164fc99c01eb8fd022af8d8
Julien Jouvent-Hallé <julien.halle@dental-wings.com>
2020-11-17 14:54:47 -0500

    give default value to branch param

--- dwdiag.ps1
+++ dwdiag.ps1
@@ -1,11 +1,11 @@
-﻿Param([string]$Branch)
+﻿Param([string]$Branch = "master")
+
 
 $testsZip = "$Env:TMP\dwdiag.zip"
 $testsDir = "$Env:TMP\dwdiag"
 
 if (Test-Path $testsDir) { Remove-Item –Path $testsDir -Recurse -Force }
 (New-Object Net.WebClient).DownloadFile("https://github.com/dentalwings/validation/archive/$Branch.zip", $testsZip)
-$zip = [System.IO.Compression.ZipFile]::Open($testsZip, 'read')
-[IO.Compression.ZipFileExtensions]::ExtractToDirectory($zip,$testsDir)
+Expand-Archive -Path $testsZip -DestinationPath $testsDir
 
-& $testsDir\run.ps1
+& $testsDir\validation-$Branch\run.ps1
