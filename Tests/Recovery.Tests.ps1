6caccbbf3a41204ce88d80a0ad129610b4ba9c23
André Düwel <duewel1982@googlemail.com>
2020-12-02 12:55:04 +0100

    add check for BackupPartitionFixed reg value

--- Tests/Recovery.Tests.ps1
+++ Tests/Recovery.Tests.ps1
@@ -1,14 +1,19 @@
 ﻿Describe "Recovery tests" -Tags "7Series", "3Series", "Medit" {
 
   $versions = (wbadmin get versions | select-String -Pattern "Version identifier: ")
+  $versionid = $versions -replace "Version identifier: "
+  $Dvolume = (wbadmin get items -version:$versionid | select-String -Pattern "mounted at D:")
 
   It "Contain one recovery image" {
     $versions.Count | should be 1
   }
 
   It "Does not incude D in the recovery" {
-    $versionid = $versions -replace "Version identifier: "
-    $Dvolume = (wbadmin get items -version:$versionid | select-String -Pattern "mounted at D:")
     $Dvolume.Count | should be 0
+  }
+  
+  It "BackupPartitionFixed RegKey is not set if D is included in recovery" {
+    $BackupPartitionFixed = Get-ItemProperty -Path "HKLM:\SOFTWARE\WOW6432Node\DWOS" -Name "BackupPartitionFixed" -ErrorAction SilentlyContinue
+    $BackupPartitionFixed -and ($Dvolume.Count -gt 0) | should be $False
   }
 }
